55bc9cf70162340b30bc340448cfc46af96bc41c
Glen Van Ginkel <glen.vanginkel@nanoporetech.com>
2020-01-16 21:49:02 +0000

    Debugging gitlab pages is not fun

--- bin/renameHosts.sh
+++ bin/renameHosts.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-for filename in ./www/cronkite/examples/reports/*.json; do
-  echo $filename
-  sed -i '' -E 's/localhost:[0-9]+/metrichor-reports.git.oxfordnanolabs.local/g' $filename
-done
+sed -i'.json' -E 's/localhost:[0-9]+/metrichor-reports.git.oxfordnanolabs.local/g' ./www/cronkite/examples/reports/*
