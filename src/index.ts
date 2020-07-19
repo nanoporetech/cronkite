6b2f27141529d5486bacee160bb0f948eb33e5d3
Glen Van Ginkel <help@metrichor.com>
2020-07-19 08:31:50 +0100

    Dependency updates

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import '@metrichor/jmespath-plus';
 import 'jmespath-edit';
 
-export { Components, JSX } from './components';
+export * from './components';
 export * from './types/datastreams.type';
 export * from './types/funnel.type';
 export * from './types/json.type';
